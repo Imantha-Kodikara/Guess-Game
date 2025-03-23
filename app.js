d2afecee7527b6c1daa42e54b3864fd808801b16
Imantha-Kodikara <imantha.l.kodikara@gmail.com>
2025-03-23 10:29:02 +0530

    final

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 let x = Math.floor((Math.random()*10)+1);
+let count = 0;
 function guessNumber(){
-    if(document.getElementById("number").value > x){
-        document.getElementById("result").innerText = "Too high";
-    }else if(document.getElementById("number").value < x){
-        document.getElementById("result").innerText = "Too low";
-    }else{
-        document.getElementById("result").innerText = "Congratulations...You won!";
-    }
+  
+        if(count < 3){
+            if(document.getElementById("number").value > x){
+                document.getElementById("result").innerText = "Too high";
+            }else if(document.getElementById("number").value < x){
+                document.getElementById("result").innerText = "Too low";
+            }else{
+                document.getElementById("result").innerText = "Congratulations...You won!";
+            }
+            count++;
+            document.getElementById("round").innerText = "Round " + count;
+        }else{
+            document.getElementById("result").innerText = "Game over";
+            let guessBtn =  document.getElementById("guessButton");
+            document.getElementById("result").innerText = "";
+            document.getElementById("round").innerText = "";
+            guessBtn.disabled = true;
+            alert("Game over... try again");
+        }
 }
+
+
+    
